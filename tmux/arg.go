12ab5ae6cd2f08b1261a0824fbd21ba2bf93500c
Jacob Koziej <jacobkoziej@gmail.com>
2021-06-20 20:08:24 -0400

    Define ArgNewWindow()

--- tmux/arg.go
+++ tmux/arg.go
@@ -15,3 +15,58 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package tmux
+
+// ArgNewWindow generates arguments to create a new window from the Window
+// receiver.
+func (w *Window) ArgNewWindow() (arg []string) {
+	if w == nil {
+		return
+	}
+
+	arg = append(arg, "new-window")
+
+	if w.nextIndexUp {
+		arg = append(arg, "-a")
+	}
+
+	// window isn't made current by default
+	if !w.makeCurrent {
+		arg = append(arg, "-d")
+	}
+
+	if w.destroyTarget {
+		arg = append(arg, "-k")
+	}
+
+	if w.printInfo {
+		arg = append(arg, "-P")
+	}
+
+	if w.startDirectory != nil {
+		arg = append(arg, "-c", *w.startDirectory)
+	}
+
+	if w.environment != nil {
+		for _, env := range w.environment {
+			arg = append(arg, "-e", env)
+		}
+	}
+
+	if w.format != nil {
+		arg = append(arg, "-F", *w.format)
+	}
+
+	if w.windowName != nil {
+		arg = append(arg, "-n", *w.windowName)
+	}
+
+	if w.targetWindow != nil {
+		arg = append(arg, "-t", *w.targetWindow)
+	}
+
+	if w.shellCommand != nil {
+		arg = append(arg, *w.shellCommand)
+	}
+
+	return
+}
